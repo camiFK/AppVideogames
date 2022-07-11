a4ba68fd06eb8d79c508db24367777493e6906a1
camila <camilafkapp@gmail.com>
2022-07-11 15:59:00 -0300

    continue

--- client/src/components/AddVideogame/AddGame.jsx
+++ client/src/components/AddVideogame/AddGame.jsx
@@ -1,36 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {useDispatch, useSelector} from 'react-redux'
-import {getGenres, postVideogame} from '../../redux/actions'
+import {getPlatforms, getGenres, postVideogame} from '../../redux/actions'
 import Styles from './AddGame.module.scss'
-
-const check = /\S+/;
-const regExpr = /^[a-z]+$/i;
-
-function validate(input) {
-    let errors = {}
-    if (!check.test(input.name) || !regExpr.test(input.name) || input.name.length < 3) {
-      errors.name = "Provide a name. Only strings (more than two characters).";
-      }
-    if(!/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g.test(input.image)) {errors.image = "Only URL directions.";}
-    if(!input.genres.length < 1) {errors.genres = "Select at least one genre.";}
-    if (!input.plataformas.length < 1) {errors.plataformas = "Select at least one plataform.";}
-  
-      return errors;
-  }
 
 const AddGame = () => {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [errors, setError] = useState({})
+    
+    useEffect(() => {
+        dispatch(getGenres())
+        dispatch(getPlatforms())
+    }, [dispatch]);
 
     const genres = useSelector(state => state.allGenres)
     const platforms = useSelector(state => state.allPlatforms)
-
-    useEffect(() => {
-        dispatch(getGenres())
-    }, [dispatch]);
+    console.log(platforms)
 
     const [input, setInput] = useState({
         name: '',
@@ -42,63 +29,124 @@ const AddGame = () => {
         plataformas: []
     })
 
-
     function handleChange(e) {
-        setInput({
-          ...input,
-          [e.target.name] : e.target.value
-        })
-        setError(validate({
-          ...input,
-          [e.target.name] : e.target.value
-        }))
+        setInput({...input, [e.target.name] : e.target.value})
       }
 
       function handleGenre(e) {
-        setInput({
-          ...input,
-          genres: [...input.genres, e.target.value]
-        })
-        setError(validate({
-            ...input,
-            [e.target.genres]: e.target.value
-        }))
+        setInput({ ...input, genres: [...input.genres, e.target.value] })
+      }
+
+      function handlePlatforms(e) {
+        setInput({ ...input, plataformas: [...input.plataformas, e.target.value] })
+      }
+
+      const handleDeleteGenre = (e) => {
+        setInput({ ...input, genres: input.genres.filter(el => el !== e)})
+      }
+
+      const handleDeletePlatform = (e) => {
+        setInput({ ...input,  plataformas: input.plataformas.filter(el => el !== e) })
+      }
+
+      const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(postVideogame(input))
+        alert('Videogame created!')
+        navigate('/home')
       }
 
 
   return (
     <div className={Styles.container}>
+
+        <form className={Styles.form} onSubmit={(e) => handleSubmit(e)}>
+
         <button onClick={() => navigate('/home')}>Home</button>
-        <h1>Create your own videogame!</h1>
+           <h1>Create your own videogame!</h1> 
+          <div className={Styles.content}>
 
-        <form className={Styles.form}>
+            <input 
+            required 
+            type="text" 
+            name="name" 
+            placeholder='Name'
+            value={input.name} 
+            onChange={handleChange} 
+            className={Styles.ctninput}/>
 
-            <label>Name:</label>
-            <input type="text" name="name" value={input.name} onChange={handleChange}/>
+            <input 
+            required 
+            type="text" 
+            name="description" 
+            placeholder='Description'
+            value={input.description} 
+            onChange={handleChange} 
+            className={Styles.ctninput}/>
 
-            <label>Description:</label>
-            <input type="text" name="description" value={input.description} onChange={handleChange}/>
+            <input 
+            required 
+            type="date" 
+            name="released" 
+            placeholder='Released'
+            value={input.released} 
+            onChange={handleChange} 
+            className={Styles.ctninput}/>
 
-            <label>Released:</label>
-            <input type="text" name="released" value={input.released} onChange={handleChange}/>
+            <input 
+            required 
+            type="number" min={1} max={5}
+            name="rating" 
+            placeholder='Rating'
+            value={input.rating} 
+            onChange={handleChange} 
+            className={Styles.ctninput}/>
 
-            <label>Rating:</label>
-            <input type="text" name="rating" value={input.rating} onChange={handleChange}/>
-
-            <label>Image:</label>
-            <input type="text" name="image" value={input.image} onChange={handleChange}/>
+            <input 
+            required 
+            type="text" 
+            name="image" 
+            placeholder='Image'
+            value={input.image} 
+            onChange={handleChange} 
+            className={Styles.ctninput}/>
 
             <label>Genres:</label>
-            <select name="genres" value={input.genres} onChange={handleGenre}>
+            <select name="genres" value={input.genres} onChange={handleGenre} className={Styles.select}>
                 <option value="">Select a genre</option>
-                {genres.map(genre => <option key={genre.id} value={genre.id}>{genre}</option>)}
+                {genres?.map(genre => <option key={genre.id} value={genres.id}>{genre}</option>)}
             </select>
 
+            <div className={Styles.genrectn}>
+              {
+                input.genres.map(genre => (
+                  <div className={Styles.selected}>
+                    <p key={genre} >{genre}</p>
+                    <button className={Styles.selectbtn} onClick={() => handleDeleteGenre(genre)}>X</button>
+                </div>
+                ))
+              }
+            </div>
+
             <label>Platforms:</label>
-            <select name="plataformas" value={input.plataformas} onChange={handleChange}>
+            <select name="plataformas" value={input.plataformas} onChange={handlePlatforms} className={Styles.select}>
                 <option value="">Select a platform</option>
-                {platforms.map(platform => <option key={platform.id} value={platform.id}>{platform}</option>)}
+                {platforms?.map(platform => <option key={platform.id} value={platform.id}>{platform}</option>)}
             </select>
+
+            <div className={Styles.platformctn}>
+              {
+                input.plataformas.map(platform => (
+                  <div className={Styles.selected}>
+                    <p key={platform} >{platform}</p>
+                    <button className={Styles.selectbtn} onClick={() => handleDeletePlatform(platform)}>X</button>
+                </div>
+                ))
+              }
+            </div>
+
+            <button className={Styles.submitbtn} type='submit'>CREATE</button>
+          </div>
 
         </form>
 
